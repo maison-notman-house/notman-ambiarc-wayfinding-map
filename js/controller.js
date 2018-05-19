74bf750118abb2e8beac7c4b75c39ffdcecb3e49
jeffyactive <jeff@reelyactive.com>
2018-05-19 14:52:24 -0400

    Changed select model from index to id

--- js/controller.js
+++ js/controller.js
@@ -6,28 +6,15 @@ angular.module('controller', [ 'reelyactive.ambiarc' ])
   // Navigation controller
   .controller('NavCtrl', function($scope, ambiarcService) {
     $scope.mapLoaded = false;
-    $scope.buildingIndex = 0;
-    $scope.floorIndex = 0;
-    $scope.poiIndex = 0;
 
     ambiarcService.load(function(ambiarc) {
       $scope.mapLoaded = true;
 
       ambiarc.registerForEvent(ambiarc.eventLabel.FloorSelected,
                                function(event) {
-        for(var cBuilding = 0; cBuilding < $scope.hierarchy.length;
-            cBuilding++) {
-          var building = $scope.hierarchy[cBuilding];
-          if(building.id === event.detail.buildingId) {
-            for(var cFloor = 0; cFloor < building.floors.length; cFloor++) {
-              if(building.floors[cFloor].id === event.detail.floorId) {
-                $scope.buildingIndex = cBuilding;
-                $scope.floorIndex = cFloor;
-                return;
-              }
-            }
-          }
-        }
+        updateBuilding(event.detail.buildingId);
+        $scope.buildingId = event.detail.buildingId;
+        $scope.floorId = event.detail.floorId;
       });
 
       ambiarcService.buildHierarchy(function(hierarchy) {
@@ -35,6 +22,9 @@ angular.module('controller', [ 'reelyactive.ambiarc' ])
         if($scope.hierarchy.length > 1) {
           $scope.showBuildingSelector = true;
         }
+        $scope.building = hierarchy[0];
+        $scope.buildingId = hierarchy[0].id;
+        $scope.floorId = hierarchy[0].floors[0].id;
         $scope.$apply();
       });
 
@@ -42,25 +32,32 @@ angular.module('controller', [ 'reelyactive.ambiarc' ])
         $scope.pois = pois;
         if($scope.pois.length > 1) {
           $scope.showPOISelector = true;
+          $scope.poiId = pois[0].id;
         }
         $scope.$apply();
       });
 
-      $scope.selectBuilding = function(buildingIndex) { };
-
-      $scope.selectFloor = function(floorIndex) {
-        var buildingId = $scope.hierarchy[$scope.buildingIndex].id;
-        var floorId = $scope.hierarchy[$scope.buildingIndex]
-                        .floors[floorIndex].id;
-        ambiarc.focusOnFloor(buildingId, floorId);
+      $scope.selectBuilding = function(buildingId) {
+        updateBuilding(buildingId);
       };
 
-      $scope.selectPOI = function(poiIndex) {
-        var mapLabelId = $scope.pois[poiIndex].id;
-        var floorId = $scope.pois[poiIndex].floorId;
-        var buildingId = $scope.pois[poiIndex].buildingId;
-        ambiarc.focusOnMapLabel(mapLabelId);
+      $scope.selectFloor = function(floorId) {
+        ambiarc.focusOnFloor($scope.buildingId, floorId);
       };
+
+      $scope.selectPOI = function(poiId) {
+        ambiarc.focusOnMapLabel(poiId);
+      };
+
+      function updateBuilding(buildingId) {
+        for(var cBuilding = 0; cBuilding < $scope.hierarchy.length;
+            cBuilding++) {
+          if($scope.hierarchy[cBuilding].id === buildingId) {
+            $scope.building = $scope.hierarchy[cBuilding];
+            return;
+          }
+        }
+      }
     });
 
   })
